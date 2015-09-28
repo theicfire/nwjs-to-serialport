e02d84690a7ad901e8af31af7a9cf5b218e98b77
Chase Lambert <chase@stripe.com>
2015-09-28 14:20:28 -0700

    better

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
-var CIRCLE_RADIUS = 5;
+var CIRCLE_RADIUS = 4;
 var MAX_THROTTLE = 1000;
-var moving = false;
+var mouse_down = false;
 $(function(ev) {
     console.log("DOM fully loaded and parsed");
     $('#circle').css({width: CIRCLE_RADIUS * 2, height: CIRCLE_RADIUS * 2});
     $(document).mousemove(function(e) {
-	    if (moving) {
+	    if (mouse_down) {
 		var throttle = px_to_throttle(e.pageX);
-		setThrottle(throttle);
+		set_throttle(throttle);
 	    }
     });
 
     $(document).mousedown(function (e) {
-	moving = true;
+	mouse_down = true;
+	var throttle = px_to_throttle(e.pageX);
+	set_throttle(throttle);
     }).mouseup(function(e) {
-	moving = false;
+	mouse_down = false;
 	recenter();
     });
+    set_throttle(0);
 });
 
 // -MAX_THROTTLE to MAX_THROTTLE
-function setThrottle(throttle) {
-    console.log('set at', throttle);
+function set_throttle(throttle) {
     if (throttle > MAX_THROTTLE) {
 	throttle = MAX_THROTTLE;
     } else if (throttle < -MAX_THROTTLE) {
 	throttle = -MAX_THROTTLE;
     }
+    console.log('set at', throttle);
     var x = throttle_to_px(throttle);
     $('#circle').css({left: x - CIRCLE_RADIUS});
 };
 
 function throttle_to_px(throttle) {
-    return throttle / 2 + MAX_THROTTLE / 2;
+    return throttle / 2 + MAX_THROTTLE / 2 + 100;
 }
 
 function px_to_throttle(px) {
-    return 2 * px - MAX_THROTTLE;
+    return 2 * (px - 100) - MAX_THROTTLE;
 }
 
 function recenter(x, y) {
-    setThrottle(0);
+    set_throttle(0);
 };
